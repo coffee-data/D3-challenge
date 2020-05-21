81210bf58c097c2d78863adc7ea9175611358ae6
coffee-data <jesse.ryan.hartman@gmail.com>
2020-05-20 21:25:09 -0500

    Left off at defining initial params

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,45 @@
-// @TODO: YOUR CODE HERE!
+// Boiler plate margin code
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Extract data from CSV
+(async function(){
+    var newsData = await d3.csv("assets/data/data.csv").catch(function(error) {
+      console.log(error);
+    });
+    // Print the tvData
+    console.log(newsData);
+
+    // Parse data and cast elements
+    // Check Chrome Dev for live update
+    newsData.forEach(function(d) {
+        d.smokes = +d.smokes;
+        d.poverty = +d.poverty;
+    });
+
+    //Create scale variables for pixel transliteration
+    // xLinearScale function
+
+  })()
